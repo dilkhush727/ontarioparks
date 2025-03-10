a0771b7be822b3eefabafeda015044512bdd7ef6
swatiopen <swati@openinnovationslab.com>
2025-03-10 15:06:10 -0400

    CC

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Auth extends CI_Controller {
 	
-	public function index()
+	public function signup()
 	{
 		$data['content'] = 'signup';
 		$this->load->view('biz/_layout', $data);
