22f70ba26c5b49d989b683620d8086f8abadbabb
Joyce Lin <ohanalin@gmail.com>
2025-03-10 14:54:08 -0400

    new files

---
+++ application/views/signup.php
@@ -0,0 +1,25 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<div class="container-fluid home-banner">
+	<div class="hero-section p-2 rounded position-relative">
+		<div class="row align-items-center">
+			<div class="col-md-6">
+				<img src="assets/images/Algonquin-Park-Backcountry-Camping.jpg" class="img-fluid mb-5">
+			</div>
+			<div class="col-md-6">
+			<!-- http://localhost/IXD-5105-0NC/ontarioparks/index.php/auth -->
+				<h1>Sign UP</h1>
+				<form action="">
+				<input type="text" id="fname" name="fname" placeholder="First Name"><br><br>
+				<input type="email" id="email" name="email" placeholder="Email"><br><br>
+			    <input type="phone" id="phone" name="phone" placeholder="Phone"><br><br>
+				</form>
+				<p>or</p>
+				<p>sign in with</p> <img src="../assets/images/google.png" alt="">
+				<p>or</p>
+				<p>Continue as Guest</p>
+				
+			</div>
+		</div>
+	</div>
+</div>
