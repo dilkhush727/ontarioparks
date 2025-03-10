b5fd7c7ff8b502a16ce1231cc43db3a12be96728
Joyce Lin <ohanalin@gmail.com>
2025-03-10 13:19:07 -0400

    commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/humber/IXD-5105-0NC/ontarioparks/';
+$config['base_url'] = 'http://localhost/IXD-5105-0NC/ontarioparks/';
 
 /*
 |--------------------------------------------------------------------------
