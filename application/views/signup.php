4753d00ad336f8bd3f423607870e9c06ee4e9bbe
Joyce Lin <ohanalin@gmail.com>
2025-03-10 19:55:05 -0400

    file update

--- application/views/signup.php
+++ application/views/signup.php
@@ -4,11 +4,11 @@
 	<div class="hero-section p-2 rounded position-relative">
 		<div class="row align-items-center">
 			<div class="col-md-6">
-				<img src="assets/images/Algonquin-Park-Backcountry-Camping.jpg" class="img-fluid mb-5">
+				<img src="../assets/images/Algonquin-Park-Backcountry-Camping.jpg" class="img-fluid mb-5">
 			</div>
 			<div class="col-md-6">
 			<!-- http://localhost/IXD-5105-0NC/ontarioparks/index.php/auth -->
-				<h1>Sign UP</h1>
+				<h1>Sign Up</h1>
 				<form action="">
 				<input type="text" id="fname" name="fname" placeholder="First Name"><br><br>
 				<input type="email" id="email" name="email" placeholder="Email"><br><br>
@@ -18,7 +18,8 @@
 				<p>sign in with</p> <img src="../assets/images/google.png" alt="">
 				<p>or</p>
 				<p>Continue as Guest</p>
-				
+				<img src="../assets/images/arrow.png" alt="">
+				<i class="fa-solid fa-circle-arrow-right"></i>
 			</div>
 		</div>
 	</div>
