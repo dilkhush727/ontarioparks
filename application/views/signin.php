8084f229718100e837fd616221a2fa79e6c6c82f
Adil surve <180300156+Daiyegamer@users.noreply.github.com>
2025-03-17 14:14:11 -0400

    create sign in page

--- application/views/signin.php
+++ application/views/signin.php
@@ -1,21 +1,25 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
-<div class="container-fluid home-banner">
+<div class="container-fluid ">
 	<div class="hero-section p-2 rounded position-relative">
 		<div class="row align-items-center">
 			<div class="col-md-6">
-				<img src="assets/images/Algonquin-Park-Backcountry-Camping.jpg" class="img-fluid mb-5">
+				<img src="<?=base_url()?>assets/images/Algonquin-Park-Backcountry-Camping.jpg" class="img-fluid mb-5">
 			</div>
 			<div class="col-md-6">
 			<!-- http://localhost/IXD-5105-0NC/ontarioparks/index.php/auth -->
-				<h1>Sign UP</h1>
+				<h1>Sign in</h1>
 				<form action="">
-				<input type="text" id="fname" name="fname" placeholder="First Name"><br><br>
+				<label for="Email">Email</label>
+				<br>
 				<input type="email" id="email" name="email" placeholder="Email"><br><br>
-			    <input type="phone" id="phone" name="phone" placeholder="Phone"><br><br>
+				<label for="password">Password</label>
+				<br>
+			    <input type="text" id="password" name="password" placeholder=""><br><br>
+				<button type="submit"> Sign in </button>
 				</form>
 				<p>or</p>
-				<p>sign in with</p> <img src="../assets/images/google.png" alt="">
+				<p>sign in with</p> <a href="https://accounts.google.com/signin" target="_blank"><img src="../assets/images/google.png" alt="Google Logo"></a>
 				<p>or</p>
 				<p>Continue as Guest</p>
 				
