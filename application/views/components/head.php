d30ef181c21ef75142fc98a595aa6943254e0871
swatiopen <swati@openinnovationslab.com>
2025-04-20 23:35:48 -0400

    CC

--- application/views/components/head.php
+++ application/views/components/head.php
@@ -1,7 +1,21 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ontario Parks</title>
+	<title>CampMate</title>
+
+    <!-- For PWA Starts -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="theme-color" content="#906172">
+    <link rel="apple-touch-icon" href="<?=base_url()?>assets/pwa/apple-touch-icon.png">
+    <link rel="manifest" href="<?=base_url()?>manifest.json">
+
+    <script type="text/javascript">
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('<?=base_url()?>sw.js');
+        }
+    </script>
+    <!-- For PWA Ends -->
 
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css">
